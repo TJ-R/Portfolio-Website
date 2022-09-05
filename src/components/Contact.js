ae08936ba862a4d1fdda962f60d97efdfaaa9d9b
TJ-R <57290416+TJ-R@users.noreply.github.com>
2022-09-05 09:30:35 -0400

    Added smooth scrolling, started contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,16 @@
+
+
 const Contact = () => {
     return (
-        <div id="contact" className="pt-28 h-screen w-full bg-neutral-50">
+        <div id="contact" className="pt-28 h-screen w-full bg-neutral-50 flex flex-row justify-center items-center text-left">
             <div>
-                <h1>Let's Connect</h1>
+                <h1 className="text-5xl">Let's Connect</h1>
+                <ul className="list-none flex flex-row space-x-10">
+                    <li>LinkedIn</li>
+                    <li>GitHub</li>
+                    <li>runyant13@gmail.com</li>
+                </ul>
             </div>
-            <ul>
-                
-            </ul>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,46 @@
+import { Link } from 'react-scroll';
 const Navbar = () => {
     return (
-        <div className="fixed top-0 bg-neutral-50 h-24 w-full flex flex-row justify-between items-center px-10">
+        <div className="fixed top-0 bg-neutral-50 h-24 w-full flex flex-row 
+                        justify-between items-center px-10 border-b border-gray-700">
             <div>
-                Thomas Runyan
+                <p className="text-xl">Thomas Runyan</p>
             </div>
             <div>
                 <ul className="list-none flex flex-row space-x-10">
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li className="hover:cursor-pointer text-xl">
+                        <Link 
+                            activeClass="active"
+                            to="about"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li className="hover:cursor-pointer text-xl">
+                        <Link 
+                            activeClass="active"
+                            to="projects"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                        >
+                            Projects
+                        </Link>
+                    </li>
+                    <li className="hover:cursor-pointer text-xl">
+                        <Link 
+                            activeClass="active"
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                        >
+                            Contact
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </div>
